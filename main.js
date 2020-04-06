294016c8e407e142179a7724a7aabd912f9da246
Ameen Sayegh <aameen951@gmail.com>
2020-04-06 19:47:24 +0300

    Improved commands.

--- main.js
+++ main.js
@@ -15,14 +15,52 @@ function usage(){
   console.log();
 }
 
-function gen_output_file(file_name, data){
+async function gen_output_file(file_name, data){
   try{
     fs.mkdirSync("./output");
   }catch(e){}
   fs.writeFileSync(`./output/${file_name}.json`, JSON.stringify(data, null, 0), "utf8");
 }
+async function read_file(file_name){
+  const result = fs.readFileSync(`./output/${file_name}.json`, "utf8");
+  return JSON.parse(result);
+}
+
+async function fetch_data()
+{
+  let data = await arcgis_download_data();
+  await gen_output_file('ksa_data_v2', gen_ksa_data_v2(data));
+}
+
+async function show_data()
+{
+  const data = await read_file('ksa_data_v2');
+
+  const entries = [
+    ...Object.entries(data.cities).map(x => x[1]).sort((a, b) => a.infections-b.infections),
+    {
+      name_eng: "TOTAL",
+      infections: data.infections,
+      recoveries: data.recoveries,
+      deaths: data.deaths,
+    }
+  ];
+  for(let city of entries)
+  {
+    console.log(city.name_eng.padEnd(20, " "), [
+      ["infections", `${city.infections}`, "\x1b[33m"],
+      ["recoveries", `${city.recoveries}`, "\x1b[32m"],
+      ["deaths", `${city.deaths}`, "\x1b[31m"],
+    ].map(x => `${x[0]}: ${x[2]}${x[1].padStart(5, " ")}\x1b[0m`).join(", "));
+  }
+  console.log(`Data Source: \x1b[34m${data.data_source}\x1b[0m`);
+  console.log(`Last Update: \x1b[32m${new Date(data.last_update).toLocaleString()}\x1b[0m`);
+}
 
 async function main(args){
+  const command = args[0];
+  const params = args.slice(1);
+  
   if(args[0] === 'gen')
   {
     const data = get_saudi_moh_data();
@@ -33,33 +71,14 @@ async function main(args){
   }
   else if(args[0] === 'fetch')
   {
-    let data = await arcgis_download_data();
+    await fetch_data();
 
-    gen_output_file('ksa_data_v2', gen_ksa_data_v2(data));
+    if(params.find(x => x == '--show'))await show_data();
   }
   else if(args[0] === 'show')
   {
-    let raw_data = await arcgis_download_data();
-    let data = gen_ksa_data_v2(raw_data);
-    const entries = [
-      ...Object.entries(data.cities).map(x => x[1]).sort((a, b) => a.infections-b.infections),
-      {
-        name_eng: "TOTAL",
-        infections: data.infections,
-        recoveries: data.recoveries,
-        deaths: data.deaths,
-      }
-    ];
-    for(let city of entries)
-    {
-      console.log(city.name_eng.padEnd(20, " "), [
-        ["infections", `${city.infections}`, "\x1b[33m"],
-        ["recoveries", `${city.recoveries}`, "\x1b[32m"],
-        ["deaths", `${city.deaths}`, "\x1b[31m"],
-      ].map(x => `${x[0]}: ${x[2]}${x[1].padStart(5, " ")}\x1b[0m`).join(", "));
-    }
-    console.log(`Data Source: \x1b[34m${data.data_source}\x1b[0m`);
-    console.log(`Last Update: \x1b[32m${new Date(data.last_update).toLocaleString()}\x1b[0m`);
+    await fetch_data();
+    await show_data();
   }
   else
   {
